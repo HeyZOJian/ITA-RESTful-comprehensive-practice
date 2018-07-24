4d35e08d7fb08ca17abcfdae7b03ce3a18c14c4a
“zojian” <hellozojian@gmail.com>
2018-07-24 22:20:59 +0800

    create employee and company model

---
+++ src/main/java/com/oocl/restfulcomprehensivepractice/RestfulComprehensivePracticeApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.restfulcomprehensivepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulComprehensivePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulComprehensivePracticeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oocl/restfulcomprehensivepractice/RestfulComprehensivePracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oocl.restfulcomprehensivepractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulComprehensivePracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
