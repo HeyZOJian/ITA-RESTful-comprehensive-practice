32e7b60573e168b33293d6e7dedae6f0ad2998f7
“zojian” <hellozojian@gmail.com>
2018-07-25 00:18:21 +0800

    implement gadd new companies in controller

--- src/main/java/com/oocl/restfulcomprehensivepractice/controller/CompanyController.java
+++ src/main/java/com/oocl/restfulcomprehensivepractice/controller/CompanyController.java
@@ -1,9 +1,12 @@
 package com.oocl.restfulcomprehensivepractice.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.oocl.restfulcomprehensivepractice.domain.Company;
 import com.oocl.restfulcomprehensivepractice.service.ICompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +19,17 @@ public class CompanyController {
     @GetMapping("/companies")
     public List<Company> getAllCompanies(){
         return companyService.getAllCompanies();
+    }
+
+    @PostMapping("/companies")
+    public JSONObject addCompany(@RequestBody Company company){
+        JSONObject res = new JSONObject();
+        if(companyService.addCompany(company)){
+            res.put("message","add company successfully");
+        }
+        else{
+            res.put("message","add company failed");
+        }
+        return res;
     }
 }
